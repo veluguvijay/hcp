a84f4b19f78e7af1634942f48bc6299be9b87ae3
veluguvijay <veluguvijay1998@gmail.com>
2025-10-08 20:11:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+  ami           = "ami-052064a798f08f0d3"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "dev-server"
+  }
+}
